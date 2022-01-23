3ee18aab2fb3c690d595f02a234d5974f34ff8f6
osmanalpercam <63802259+OsmanAlperCAM@users.noreply.github.com>
2022-01-23 15:20:54 +0300

    fetching data from api with axios

---
+++ src/hooks/useFetch/useFetch.tsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {CountryType} from '../../types/country';
+
+const baseUrl: string = 'https://restcountries.com/v3.1/';
+
+const useFetch = (url: string) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [countries, setCountries] = useState<CountryType[]>([]);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const {data} = await axios.get<CountryType[]>(`${baseUrl}${url}`);
+      setCountries(data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return {countries, loading, error, fetchData};
+};
+export default useFetch;
